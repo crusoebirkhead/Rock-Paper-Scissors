3d661e10d88badfbc7a523e8560b59d74a40cd3d
crusoebirkhead <crusoebirkhead@gmail.com>
2021-08-12 18:48:07 -0700

    complete rework of if/else statement, added preliminary DOM manipulator to display text for winning/losing

--- script.js
+++ script.js
@@ -1,74 +1,55 @@
-//Global scope
-let hand = ['Rock', 'Paper', 'Scissors'];
-let playerSelection = ''
-let playerScore = 0
-let computerScore = 0
-let score = document.getElementById('score').innerHTML 
-
-//Computer's selection
-
-function computerChoice(){
-  let randomIndex = Math.floor(Math.random() * hand.length);
-  let unknownHand = hand[randomIndex];
-  return (unknownHand);
+document.getElementById('Rock').onclick = user;
+document.getElementById('Paper').onclick = user;
+document.getElementById('Scissors').onclick = user;
+ 
+ 
+function user(){
+   let userChoice = this.id;
+   // console.log("User: " + userChoice)
+ 
+   let computerChoice = Math.random();
+        if (computerChoice < 0.34) {
+            computerChoice = "rock";
+         }else if(computerChoice <= 0.67) {
+            computerChoice = "paper";
+         }else{
+            computerChoice = "scissors";
+         };
+ 
+   // console.log("Computer: " + computerChoice);
+ 
+   // console.log(compare(userChoice, computerChoice));
+ 
+   function compare(choice1, choice2) {
+  
+       if (choice1 === choice2) {
+           return "The result is a tie!";
+       }
+       if (choice1 === "rock") {
+           if (choice2 === "scissors") {
+               return "rock wins";
+           }else{
+               return "paper wins";
+           }
+       }
+        if (choice1 === "paper") {
+            if (choice2 === "rock") {
+                return "paper wins";
+             } else {
+                 return "scissors wins";
+             }
+        }
+  
+        if (choice1 === "scissors") {
+            if (choice2 === "rock") {
+                return "rock wins";
+            } else {
+                return "scissors wins";
+           }
+       }
+   }
 }
-
-
-// User's selection
-
-let rock = document.getElementById('Rock')
-rock.onclick = function (e){
-  playerSelection = 'Rock'
-  console.log(playerSelection)
-}
-
-let paper = document.getElementById('Paper')
-paper.onclick = function (e){
-  playerSelection = 'Paper'
-  console.log(playerSelection)
-}
-
-let scissors = document.getElementById('Scissors')
-scissors.onclick = function (e){
-  playerSelection = 'Scissors'
-  console.log(playerSelection)
-}
-
-
-
-
-//Decides winner and counts score
-function responseReturn(playerSelection, randomHand){
-
-  randomHand = computerChoice()
-
-  if (((playerSelection === "Rock" && randomHand === "Paper") ||
-  (playerSelection === "Paper" && randomHand === "Scissors") ||
-  (playerSelection === "Scissors" && randomHand === "Rock"))){
-
-    computerScore += 1
-    let computerWon = document.createTextNode("Computer won!")
-    score.appendChild(computerWon)
-
-
-    if (playerScore = 5){
-      let winner = document.createTextNode("You have won the game!");
-      score.appendChild(winner)
-    }
-
-  else if (playerSelection === randomHand){
-  let tie = document.createTextNode("It's a tie")
-  score.appendChild(tie)
-  }
-    else{
-      playerScore += 1
-      let userWon = document.createTextNode("User won")
-      score.appendChild(userWon)
-
-      if (computerScore = 5){
-        score = "You have lost the game!"
-      }
-    }
-}
-}
-
+ var paragraph = document.getElementById("score");
+var text = document.createTextNode("This just got added");
+ 
+paragraph.appendChild(text);
