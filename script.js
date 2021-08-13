9a9cbf0220b0755b1d8c5b8d9f25b91642bde125
crusoebirkhead <crusoebirkhead@gmail.com>
2021-08-12 19:49:19 -0700

    Result messages are now displayed, keeping count of score is in the works

--- script.js
+++ script.js
@@ -1,55 +1,89 @@
-document.getElementById('Rock').onclick = user;
-document.getElementById('Paper').onclick = user;
-document.getElementById('Scissors').onclick = user;
- 
- 
+document.getElementById('rock').onclick = user;
+
+document.getElementById('paper').onclick = user;
+
+document.getElementById('scissors').onclick = user;
+
+let score = document.getElementById('score')
+
+
+
+
+//User choice
 function user(){
-   let userChoice = this.id;
-   // console.log("User: " + userChoice)
- 
-   let computerChoice = Math.random();
-        if (computerChoice < 0.34) {
-            computerChoice = "rock";
-         }else if(computerChoice <= 0.67) {
-            computerChoice = "paper";
-         }else{
-            computerChoice = "scissors";
-         };
- 
-   // console.log("Computer: " + computerChoice);
- 
-   // console.log(compare(userChoice, computerChoice));
- 
-   function compare(choice1, choice2) {
+
+    let userChoice = this.id;
+
+    console.log("User: " + userChoice)
+
+//Computer choice
+    let computerChoice = Math.random();
+
+         if (computerChoice < 0.34) {
+
+             computerChoice = "rock";
+
+          }else if(computerChoice <= 0.67) {
+
+             computerChoice = "paper";
+
+          }else{
+
+             computerChoice = "scissors";
+
+          }; 
+
+//Decides winner and posts announcement
+    function compare(choice1, choice2) {
+    
+      if (choice1 === choice2) {
+
+          score.textContent += "   The result is a tie!";
+      }
+
+      if (choice1 === "rock" && choice2 === "scissors") {
+            playerScore++;
+
+            score.textContent += "    User wins";
+          }
+      
+      if (choice1 === "rock" && choice2 === "paper") {
+            computerScore++;
+
+            score.textContent += "   Computer wins";
+
+          }
+      
+
+      if (choice1 === "paper" && choice2 === "rock") { 
+            playerScore++;
+
+            score.textContent += " User wins";
+
+          }
+
+      if (choice1 === "paper" && choice2 === "scissors") { 
+            computerScore++;
+
+            score.textContent += " Computer wins";
+
+          }
   
-       if (choice1 === choice2) {
-           return "The result is a tie!";
-       }
-       if (choice1 === "rock") {
-           if (choice2 === "scissors") {
-               return "rock wins";
-           }else{
-               return "paper wins";
-           }
-       }
-        if (choice1 === "paper") {
-            if (choice2 === "rock") {
-                return "paper wins";
-             } else {
-                 return "scissors wins";
-             }
-        }
+      if (choice1 === "scissors" && choice2 === "rock") { 
+            computerScore++;
+
+            score.textContent += " Computer wins";
+
+           } 
+
+      if (choice1 === "scissors" && choice2 === "paper") { 
+            playerScore++;
+
+            score.textContent += " User wins";
+
+           } 
+    } 
+  }
+
   
-        if (choice1 === "scissors") {
-            if (choice2 === "rock") {
-                return "rock wins";
-            } else {
-                return "scissors wins";
-           }
-       }
-   }
-}
- var paragraph = document.getElementById("score");
-var text = document.createTextNode("This just got added");
- 
-paragraph.appendChild(text);
+  
