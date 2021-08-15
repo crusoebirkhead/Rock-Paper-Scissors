ad7a05234b94e43edc0aadac6e983f6368b087b0
Crusoe Birkhead <81332371+crusoebirkhead@users.noreply.github.com>
2021-08-14 23:26:17 -0700

    Add files via upload
    Final commit for project. JS now posts score to main page and alerts who won the round. Had to push this through file upload instead of CLI, reads back corrupted under git status :(

--- script.js
+++ script.js
@@ -1,89 +1,79 @@
 document.getElementById('rock').onclick = user;
-
 document.getElementById('paper').onclick = user;
-
 document.getElementById('scissors').onclick = user;
+let player = document.getElementById('player-result');
+let computer = document.getElementById('computer-result');
+let tie = document.getElementById('tie')
+let playerScore = 0;
+let computerScore = 0;
 
-let score = document.getElementById('score')
-
-
-
-
-//User choice
 function user(){
-
-    let userChoice = this.id;
-
+    var userChoice = this.id;
     console.log("User: " + userChoice)
 
-//Computer choice
-    let computerChoice = Math.random();
-
+    var computerChoice = Math.random();
          if (computerChoice < 0.34) {
-
              computerChoice = "rock";
-
           }else if(computerChoice <= 0.67) {
-
              computerChoice = "paper";
-
           }else{
-
              computerChoice = "scissors";
-
           }; 
 
-//Decides winner and posts announcement
+    console.log("Computer: " + computerChoice);
+
+    console.log(compare(userChoice, computerChoice)); 
+
     function compare(choice1, choice2) {
-    
-      if (choice1 === choice2) {
+      document.getElementById('player-score').innerHTML = playerScore
+		    if (choice1 === choice2) {
+             tie.textContent += "   The result is a tie!";
+             document.getElementById('player-score').innerHTML = "Player score:" + playerScore;
+             document.getElementById('computer-score').innerHTML = "Computer score:" + computerScore;
+        }
+ 
+        if (choice1 === "rock" && choice2 === "scissors") {
+              playerScore++;
+              document.getElementById('player-score').innerHTML = "Player score:" + playerScore;
+              document.getElementById('computer-score').innerHTML = "Computer score:" + computerScore;
+              player.textContent += "    Player wins";
+            }
+        if (choice1 === "rock" && choice2 === "paper") {
+              computerScore++;
+              document.getElementById('computer-score').innerHTML = "Computer score:" + computerScore;
+              document.getElementById('player-score').innerHTML = "Player score:" + playerScore;
+              computer.textContent += "   Computer wins";
+            }
+				
+ 
+         if (choice1 === "paper" && choice2 === "rock") {
+              playerScore++;
+              document.getElementById('player-score').innerHTML = "Player score:" + playerScore;
+              document.getElementById('computer-score').innerHTML = "Computer score:" + computerScore;
+              player.textContent += "    Player wins";
+              } 
+              if (choice1 === "paper" && choice2 === "scissors") {
+                computerScore++;
+                document.getElementById('computer-score').innerHTML = "Computer score:" + computerScore;
+                document.getElementById('player-score').innerHTML = "Player score:" + playerScore;
+                computer.textContent += "   Computer wins";
+              }
+		     
+		
+		     if (choice1 === "scissors" && choice2 === "paper") {
+              playerScore++;
+              document.getElementById('player-score').innerHTML = "Player score:" + playerScore;
+              document.getElementById('computer-score').innerHTML = "Computer score:" + computerScore;
+              player.textContent += "    Player wins";
+             } 
+             if (choice1 === "scissors" && choice2 === "rock") {
+              computerScore++;
+              document.getElementById('computer-score').innerHTML = "Computer score:" + computerScore;
+              document.getElementById('player-score').innerHTML = "Player score:" + playerScore;
+              computer.textContent += "   Computer wins";
+            }
+        }
+    }
 
-          score.textContent += "   The result is a tie!";
-      }
 
-      if (choice1 === "rock" && choice2 === "scissors") {
-            playerScore++;
-
-            score.textContent += "    User wins";
-          }
-      
-      if (choice1 === "rock" && choice2 === "paper") {
-            computerScore++;
-
-            score.textContent += "   Computer wins";
-
-          }
-      
-
-      if (choice1 === "paper" && choice2 === "rock") { 
-            playerScore++;
-
-            score.textContent += " User wins";
-
-          }
-
-      if (choice1 === "paper" && choice2 === "scissors") { 
-            computerScore++;
-
-            score.textContent += " Computer wins";
-
-          }
-  
-      if (choice1 === "scissors" && choice2 === "rock") { 
-            computerScore++;
-
-            score.textContent += " Computer wins";
-
-           } 
-
-      if (choice1 === "scissors" && choice2 === "paper") { 
-            playerScore++;
-
-            score.textContent += " User wins";
-
-           } 
-    } 
-  }
-
-  
   
